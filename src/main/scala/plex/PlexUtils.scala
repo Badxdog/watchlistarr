79001d51a00c63e62cd87d96374abec3f058c01b
Badxdog <272027771+Badxdog@users.noreply.github.com>
2026-04-02 22:06:53 +0200

    Fix Plex genre extraction and Synology Docker build

--- src/main/scala/plex/PlexUtils.scala
+++ src/main/scala/plex/PlexUtils.scala
@@ -213,13 +213,16 @@ trait PlexUtils {
       .unsafeFromString(s"https://discover.provider.plex.tv$key")
       .withQueryParam("X-Plex-Token", config.plexTokens.headOption.getOrElse("unknown"))
 
-    val guids: EitherT[IO, Throwable, List[String]] = for {
+    val metadata: EitherT[IO, Throwable, TokenWatchlist] = for {
       response <- EitherT(client.httpRequest(Method.GET, url))
       result   <- EitherT(IO.pure(response.as[TokenWatchlist])).leftMap(err => new Throwable(err))
-      guids = result.MediaContainer.Metadata.flatMap(_.Guid.map(_.id))
-    } yield guids
+    } yield result
 
-    guids.map(ids => Item(i.title, ids, i.`type`, ended = None, genres = i.Genre.map(_.tag).toSet))
+    metadata.map { result =>
+      val ids    = result.MediaContainer.Metadata.flatMap(_.Guid.map(_.id))
+      val genres = result.MediaContainer.Metadata.flatMap(_.Genre.map(_.tag)).toSet
+      Item(i.title, ids, i.`type`, ended = None, genres = genres)
+    }
   }
 
   private def cleanKey(path: String): String =
